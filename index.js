ccf0290cd1bf42c8649fc6a4cc1ddb3c5ee6e3e8
Christopher Fujino <christopherfujino@gmail.com>
2016-06-28 16:01:10 -1000

    started installer node script

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 let fs = require('fs');
 
+let inventory = fs.readFile('inventory.json', 'utf8', installer);
 
+function installer(err, data) {
+  if (err) throw err;
+  let dotfiles = JSON.parse(data).dotfiles;
+
+  console.log('\n' + splash('Welcome To Chris\' Dotfiles Installer'));
+  dotfiles.forEach(function(dotfile, i){
+    console.log(i + '. ' + dotfile.name);
+  });
+}
+
+function splash(string) {
+  let topBottom = '';
+  for(let i=0; i<string.length+4; i++) {
+    topBottom += '*';
+  }
+  return topBottom + '\n* ' + string + ' *\n' + topBottom + '\n'; 
+}
