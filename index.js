08852dfb9b9ab1c9d6cfced972ff5dccd031c20a
Christopher Fujino <christopherfujino@gmail.com>
2016-08-28 02:54:25 -1000

    more formatting, added quit functionality, os check

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 
 let fs = require('fs')
 let dotfiles = {}
+let {exec} = require('child_process')
+let {platform} = require('os')
 
 fs.readFile('inventory.json', 'utf8', installer)
 
 function menu () {
-  console.log('\n' + splash('Welcome To Chris\' Dotfiles Installer'))
+  console.log('\033[2J\033c')
+  console.log(splash('Welcome To Chris\' Dotfiles Installer'))
   dotfiles.forEach(function (dotfile, i) {
-    let check = ' '
-    if (dotfile.checked) {check = 'x'}
-    console.log(`${i}. ${dotfile.name} [${check}]`)
+    if (dotfile.platform === 'any' || dotfile.platform === platform()) {
+      let check = ' '
+      if (dotfile.checked) {check = 'x'}
+      console.log(`${i}. ${dotfile.name} [${check}]`)
+    }
   })
+  console.log('\nType number to select/unselect dotfile, [q] to quit, [i] to install:')
 }
 
 function installer (err, data) {
@@ -30,6 +36,14 @@ function splash (string) {
 
 let stdin = process.openStdin()
 stdin.on('data', function (chunk) {
+  if (('' + chunk)[0] === 'q') {
+    console.log('Exiting without installing dotfiles.')
+    process.exit(0)
+  }
+  if (('' + chunk)[0] === 'i') {
+    console.log('install!')
+    return
+  }
   chunk = +chunk
   if (isNaN(chunk) || chunk >= dotfiles.length) return
   dotfiles[chunk].checked = !dotfiles[chunk].checked
