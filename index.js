d99d64bd194c6a4961e6d28c86d3ec4b8240c5e0
Christopher Fujino <christopherfujino@gmail.com>
2016-08-27 21:16:11 -1000

    brought to standardjs compliance, use string templates

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-let fs = require('fs');
+#!/usr/bin/env node
 
-let inventory = fs.readFile('inventory.json', 'utf8', installer);
+let fs = require('fs')
 
-function installer(err, data) {
-  if (err) throw err;
-  let dotfiles = JSON.parse(data).dotfiles;
+fs.readFile('inventory.json', 'utf8', installer)
 
-  console.log('\n' + splash('Welcome To Chris\' Dotfiles Installer'));
-  dotfiles.forEach(function(dotfile, i){
-    console.log(i + '. ' + dotfile.name);
-  });
+function installer (err, data) {
+  if (err) throw err
+  let dotfiles = JSON.parse(data).dotfiles
+
+  console.log('\n' + splash('Welcome To Chris\' Dotfiles Installer'))
+  dotfiles.forEach(function (dotfile, i) {
+    console.log(`${i}. ${dotfile.name}`)
+  })
 }
 
-function splash(string) {
-  let topBottom = '';
-  for(let i=0; i<string.length+4; i++) {
-    topBottom += '*';
+function splash (string) {
+  let topBottom = ''
+  for (let i = 0; i < string.length + 4; i++) {
+    topBottom += '*'
   }
-  return topBottom + '\n* ' + string + ' *\n' + topBottom + '\n'; 
+  return `${topBottom}\n* ${string} *\n${topBottom}\n`
 }
+
+let stdin = process.openStdin()
+stdin.on('data', function (chunk) {
+})
