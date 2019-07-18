ec81123ca4b4a049638c3bb5dd5d1829a05a3a6a
Christopher Fujino <fujino@google.com>
2019-07-18 09:43:49 -0700

    add show-path.sh and chown-homebrew-dirs.sh scripts

---
+++ scripts/chown-homebrew-dirs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $(uname) != 'Darwin' ]; then
+  echo "This script only supports Homebrew on mac."
+  exit 1
+fi
+
+echo 'sudo chown -R $(whoami) /usr/local/bin /usr/local/etc /usr/local/sbin /usr/local/share /usr/local/share/doc'
+
+read -p 'Hit enter to proceed...'
+
+sudo chown -R $(whoami) /usr/local/bin /usr/local/etc /usr/local/sbin /usr/local/share /usr/local/share/doc
