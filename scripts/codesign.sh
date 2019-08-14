08813ebc178a3ec8e6b9920d8b5b0f211adb4541
Christopher Fujino <christopherfujino@gmail.com>
2019-08-14 11:25:05 -0700

    clean up codesign.sh

--- scripts/codesign.sh
+++ scripts/codesign.sh
@@ -56,25 +56,6 @@ function saveUuid {
   REQUEST_UUID=$(echo "$1" | sed -n 's/.*RequestUUID = \([a-z0-9-]*\).*/\1/p')
 }
 
-STRING0="2019-08-13 12:56:18.932 altool[62341:349762] No errors uploading 'dart.zip'.
-RequestUUID = f4416803-b274-4cfb-8d77-ef5432803588"
-
-STRING1="2019-08-13 12:58:23.494 altool[62643:351373] No errors getting notarization info.
-
-   RequestUUID: f4416803-b274-4cfb-8d77-ef5432803588
-          Date: 2019-08-13 19:56:19 +0000
-        Status: in progress
-    LogFileURL: (null)"
-
-STRING2="2019-08-13 12:58:52.037 altool[62662:351763] No errors getting notarization info.
-
-   RequestUUID: f4416803-b274-4cfb-8d77-ef5432803588
-          Date: 2019-08-13 19:56:19 +0000
-        Status: success
-    LogFileURL: https://osxapps-ssl.itunes.apple.com/itunes-assets/Enigma113/v4/65/f6/e9/65f6e917-a948-31de-438a-5b2e93814862/developer_log.json?accessKey=1565920731_2821058595715934222_4x3rADg4T2VgB%2BEzpGF%2BB%2FcadsLjY%2Boaz1Hflx%2Bd%2BuRjtk9pPpp3zv3BDrozFll%2BIQLbRDSxjLkvjPlxU5103UpKUCYjdQF5r9LaJG%2BgyPv36yFOMF0trEsAk%2Bl53X2mKoHOFVhKtQJnSP1Hi66W7c2mWamkFfl4iwgfT68BgP0%3D
-   Status Code: 0
-Status Message: Package Approved"
-
 function sign {
   # force sign, add secure timestamp and hardened runtime
   codesign -f -s "$CODESIGN_CERT_NAME" "$1" --timestamp --options=runtime
@@ -136,8 +117,3 @@ sign "$TARGET_PATH"
 zipTarget "$TARGET_PATH"
 notarize "$TARGET_ZIP"
 #staple "$TARGET_PATH"
-
-#saveUuid "$STRING0"
-#
-#verifyStatus "$STRING1"
-#verifyStatus "$STRING2"
