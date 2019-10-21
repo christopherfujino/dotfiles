3141d5590d34edf3ae3648257f9d6378ddbf5f80
Christopher Fujino <fujino@google.com>
2019-10-21 12:47:27 -0700

    edit run-flutter-recipe.sh

--- scripts/run-flutter-recipe.sh
+++ scripts/run-flutter-recipe.sh
@@ -2,16 +2,26 @@
 
 cd "$HOME/git/chrome_infra/build/scripts/slave/recipes/flutter"
 
+UNAME=$(uname)
+
+if [ $UNAME == 'darwin' ]; then
+  OPEN='open'
+elif [ $UNAME == 'linux' ]; then
+  OPEN='xdg-open'
+else
+  OPEN='echo'
+fi
+
 if [ -z $1 ]; then
   echo Please specify package name
   exit 1
 fi
 
-led get-builder 'luci.flutter.prod:Mac' |\
+led get-builder 'luci.flutter.prod:ios-deploy' |\
   led edit -p 'revision="123abc"' |\
   led edit -p "package_name=\"$1\"" |\
   led edit-recipe-bundle |\
   led launch |\
   tail -n 1 |\
   grep -o 'https.*$' |\
-  xargs open
+  xargs $OPEN
