e7db7ff38b84958ae855c5ea7c66b9bc23f47523
Christopher Fujino <christopherfujino@gmail.com>
2020-03-13 12:11:36 -0700

    cleanup

--- scripts/compare_commits.py
+++ scripts/compare_commits.py
@@ -4,34 +4,40 @@
 import subprocess
 import sys
 
+
 def show_fuller(revision):
     '''Show both author date & commit date of revision'''
     subprocess.call([
         'git',
         'show',
         revision,
-        '-s', # suppress diff
-        '--pretty=fuller', # show both dates
+        '-s',   # suppress diff
+        '--pretty=fuller',  # show both dates
         ])
+
 
 def main(first_revision, second_revision):
     '''Main execution'''
-    print first_revision
-    print second_revision
-    #subprocess.call([
+    print(first_revision)
+    print(second_revision)
     output = subprocess.check_output([
         'git',
         'rev-list',
         '--date-order',
         '--all',
         ]).split('\n')
-    smaller_index = min(output.index(first_revision), output.index(second_revision))
-    greater_index = max(output.index(first_revision), output.index(second_revision))
+    smaller_index = min(
+            output.index(first_revision),
+            output.index(second_revision))
+    greater_index = max(
+            output.index(first_revision),
+            output.index(second_revision))
     show_fuller(output[greater_index])
     show_fuller(output[smaller_index])
 
+
 if len(sys.argv) != 3:
-    print 'Usage error!'
-    exit(1)
+    print('Usage error!')
+    sys.exit(1)
 
 main(sys.argv[1], sys.argv[2])
