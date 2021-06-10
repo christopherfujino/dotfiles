7d45ed117786de14c8cc0ef74b03528482c86c29
Christopher Fujino <christopherfujino@gmail.com>
2021-06-10 12:57:35 -0700

    add DART_REVISION to conductor.sh

--- scripts/conductor.sh
+++ scripts/conductor.sh
@@ -10,9 +10,10 @@ fi
 
 export FRAMEWORK_MIRROR='git@github.com:christopherfujino/flutter.git'
 export ENGINE_MIRROR='git@github.com:christopherfujino/engine.git'
-export CANDIDATE_BRANCH='flutter-1.27-candidate.12'
-export RELEASE_CHANNEL='beta'
-export ENGINE_CHERRYPICKS='48cf2b937,95cb19f9b'
-export FRAMEWORK_CHERRYPICKS='781eb13,6afbb25a4,d75c2781,a7f7687a'
+export CANDIDATE_BRANCH='flutter-2.2-candidate.10'
+export RELEASE_CHANNEL='dev'
+#export ENGINE_CHERRYPICKS='eb658e840d92ab4e8a9862e912ab6a4351f5aa90,07fa0dc8ab532030a798fff45d18fb23338a86f8'
+export FRAMEWORK_CHERRYPICKS='302e992ca7d5a16e1e44910ab98a7526f0d2d829,485c409184777c9a5eac0d65c362e00d080ea310,43d22ae2d38445e96ec865522da2e228ef392c4d'
+export DART_REVISION='516eb3b05c252ef3d34255d06700c1f09f7f4502'
 
 "$CONDUCTOR_PATH" $@
