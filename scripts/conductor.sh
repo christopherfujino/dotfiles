2b7b37e0c7767aa2c4e675c717e97e4fe9a180a0
Christopher Fujino <christopherfujino@gmail.com>
2021-08-01 09:49:11 -0700

    add better comments to conductor.sh

--- scripts/conductor.sh
+++ scripts/conductor.sh
@@ -10,10 +10,18 @@ fi
 
 export FRAMEWORK_MIRROR='git@github.com:christopherfujino/flutter.git'
 export ENGINE_MIRROR='git@github.com:christopherfujino/engine.git'
-export CANDIDATE_BRANCH='flutter-2.2-candidate.10'
-export RELEASE_CHANNEL='dev'
-#export ENGINE_CHERRYPICKS='eb658e840d92ab4e8a9862e912ab6a4351f5aa90,07fa0dc8ab532030a798fff45d18fb23338a86f8'
-export FRAMEWORK_CHERRYPICKS='302e992ca7d5a16e1e44910ab98a7526f0d2d829,485c409184777c9a5eac0d65c362e00d080ea310,43d22ae2d38445e96ec865522da2e228ef392c4d'
-export DART_REVISION='516eb3b05c252ef3d34255d06700c1f09f7f4502'
+export CANDIDATE_BRANCH='flutter-2.4-candidate.4'
+export RELEASE_CHANNEL='beta'
+export ENGINE_CHERRYPICKS='288983cea003e43037c306f558c7d9de970484c8'
+#export FRAMEWORK_CHERRYPICKS=''
+#export DART_REVISION='065872e802209ffa97aa3fad204758830b6100a0'
+
+# For incrementing:
+#   - x major stable release bump
+#   - y first dev release after a beta
+#   - z stable hotfix release
+#   - m initial dev release
+#   - n beta hotfix release
+export INCREMENT='n'
 
 "$CONDUCTOR_PATH" $@
