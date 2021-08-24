18e6b723e87a8f1e1e7a206306a0d0a8e400cc43
Christopher Fujino <christopherfujino@gmail.com>
2021-08-24 14:08:10 -0700

    update most-commits.rb for this perf

--- scripts/most-commits.rb
+++ scripts/most-commits.rb
@@ -1,24 +1,32 @@
 #!/usr/bin/env ruby
 
-if ARGV.length < 3
-  puts 'Usage: most-commits.sh [filename] [since] [until]'
+if ARGV.length < 2
+  puts 'Usage: most-commits.sh $since $until [$filename]'
   exit 1
 end
 
-file = ARGV[0]
-since = ARGV[1]
-until_date = ARGV[2]
+since = ARGV[0]
+until_date = ARGV[1]
+file = ARGV[2]
+
 # Have values default to 0
 tallies = Hash.new(0)
 
-# Get an array of all hashes between [since] and [until_date] that changed
-# [file].
-hashes = `git log --pretty=format:"%H" --since="#{since}" --until="#{until_date}" -- "#{file}"`.strip
-hashes.lines.each do |hash|
+hashes = if file.nil?
+           `git log --pretty=format:"%H" --since="#{since}" --until="#{until_date}"`
+         else
+           # Get an array of all hashes between [since] and [until_date] that
+           # changed [file].
+           `git log --pretty=format:"%H" --since="#{since}" --until="#{until_date}" -- "#{file}"`
+         end.lines
+hashes.each do |hash|
   # Get author of [hash].
   author = `git show --no-patch --pretty=%an "#{hash.strip}"`.strip
   tallies[author] += 1
+  #if author =~ /Fujino/
+  #  puts `git show --no-patch --format=oneline #{hash}`
+  #end
 end
 # Sort, in ascending order, by commit count.
 puts tallies.sort_by {|k, v| v}.map {|k, v| "#{k} - #{v}"}
-puts "Total: #{hashes.lines.length}"
+puts "Total: #{hashes.length}"
