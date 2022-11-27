27e45302fae1e207405a465dcfd347a7c6ad88c4
Christopher Fujino <christopherfujino@gmail.com>
2022-11-27 14:00:57 -0800

    have ensure_flutter_goma_setup.sh exports env var

--- scripts/ensure_flutter_goma_setup.sh
+++ scripts/ensure_flutter_goma_setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-readonly GOMA_AUTH="$HOME/flutter_goma/goma_auth.py"
-readonly GOMA_CTL="$HOME/flutter_goma/goma_ctl.py"
-readonly GOMACC="$HOME/flutter_goma/gomacc"
-readonly GOMA_HTTP2_PROXY="$HOME/flutter_goma/http_proxy"
+set -uo pipefail
+
+export GOMA_DIR="$HOME/flutter_goma"
+readonly GOMA_AUTH="$GOMA_DIR/goma_auth.py"
+readonly GOMA_CTL="$GOMA_DIR/goma_ctl.py"
+readonly GOMACC="$GOMA_DIR/gomacc"
+readonly GOMA_HTTP2_PROXY="$GOMA_DIR/http_proxy"
 readonly GOMA_HTTP2_PROXY_PORT="8199"
 GOMA_CACHE_DIR="$HOME/flutter_goma_cache"
 # The URL of the backend cannot be read from prebuilt. Hard-code it for now.
@@ -11,8 +14,8 @@ BACKEND_URL="rbe-prod1.endpoints.fuchsia-infra-goma-prod.cloud.goog"
 
 # Download client. Assumes cipd from depot_tools is on path.
 echo 'fuchsia/third_party/goma/client/${platform}  release'
-echo 'fuchsia/third_party/goma/client/${platform}  release' | cipd ensure -ensure-file - -root $HOME/flutter_goma
+echo 'fuchsia/third_party/goma/client/${platform}  release' | cipd ensure -ensure-file - -root $GOMA_DIR
 
 # Authenticate
-$HOME/flutter_goma/goma_auth login
+$GOMA_DIR/goma_auth login
 GOMA_LOCAL_OUTPUT_CACHE_DIR="$GOMA_CACHE_DIR" "$GOMA_CTL" ensure_start
