454aeef092097efc3350fe440571941ecc2a1418
Christopher Fujino <christopherfujino@gmail.com>
2024-05-11 15:54:16 -0700

    add menu for lock, suspend, and shutdown to polybar

--- launch_polybar.sh
+++ launch_polybar.sh
@@ -2,13 +2,21 @@
 
 set -euo pipefail
 
+POLYBAR_DEBUGGING='' # false
+# POLYBAR_DEBUGGING='true'
+
+BAR_NAME='my_great_bar'
+
 # Terminate already running bar instances
 # If all your bars have ipc enabled, you can use
 polybar-msg cmd quit || true
 
 for m in $(xrandr --query | grep ' connected' | awk '{print $1}'); do
-  MONITOR=$m polybar my_great_bar & disown
-  #polybar my_great_bar 2>&1 | tee -a /tmp/polybar.log & disown
+  if [[ -n "$POLYBAR_DEBUGGING" ]]; then
+    LOG_FILE="/tmp/polybar${m}.log"
+    echo "Launching polybar with debug logging at $LOG_FILE"
+    MONITOR=$m polybar "$BAR_NAME" 2>&1 | tee -a "$LOG_FILE" & disown
+  else
+    MONITOR=$m polybar "$BAR_NAME" 2>&1 >/dev/null & disown
+  fi
 done
-
-echo 'polybar launched...'
