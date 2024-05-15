fee1f426992e941d271edb5c34cd98892a60265b
Christopher Fujino <christopherfujino@gmail.com>
2024-05-14 23:05:25 -0700

    finish fleshing out init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,37 @@
--- Experimental
+---- Experimental
 
--- bootstrap lazy.nvim
+-- options
+vim.opt.number = true
+vim.opt.tabstop = 2        -- Width of a <tab>
+vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of auto-indent
+vim.opt.expandtab = true   -- Use the appropriate number of spaces to insert a tab
+vim.opt.signcolumn = "yes" -- always draw sign column
+vim.opt.list = true        -- show "-" for trailing whitespace, ">" for tab
+
+-- better splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+---- mappings
+
+-- disable middle mouse paste
+vim.keymap.set({"n", "v", "i"}, "<MiddleMouse>", "<Nop>", {noremap=true})
+vim.keymap.set({"n", "v", "i"}, "<2-MiddleMouse>", "<Nop>", {noremap=true})
+vim.keymap.set({"n", "v", "i"}, "<3-MiddleMouse>", "<Nop>", {noremap=true})
+vim.keymap.set({"n", "v", "i"}, "<4-MiddleMouse>", "<Nop>", {noremap=true})
+
+vim.keymap.set("n", "<c-p>", ":Files<cr>", {noremap=true})
+
+---- plugins
+
+--bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
-    "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
@@ -17,24 +40,40 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(
   -- plugins
   {
+    -- Chris Kempson's Base16 colorschemes, see `colorscheme...`
+    "chriskempson/base16-vim",
+
     "neovim/nvim-lspconfig",
     "mfussenegger/nvim-dap",
+    -- Shows git diff in gutter
+    "airblade/vim-gitgutter",
+    -- :Rename, :Move, :Delete
+    "tpope/vim-eunuch",
+    -- cs'"
+    "tpope/vim-surround",
+    -- auto-bracket pairing
+    "windwp/nvim-autopairs",
+
+    -- auto-complete
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+
+    -- git
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
+
+    'junegunn/fzf',
+    'junegunn/fzf.vim',
   },
   -- options
   {}
 )
 
--- options
-vim.opt.number = true
-vim.opt.tabstop = 2       -- Width of a <tab>
-vim.opt.shiftwidth = 2    -- Number of spaces to use for each step of auto-indent
-vim.opt.expandtab = true  -- Use the appropriate number of spaces to insert a tab
-vim.opt.signcolumn = "yes" -- always draw sign column
-vim.cmd.colorscheme("retrobox")
+vim.cmd.colorscheme("base16-eighties")
 
--- better splits
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+require("nvim-autopairs").setup {
+  disable_filetype = {},
+}
 
 -- lsp
 local opts = { noremap=true, silent=true }
@@ -60,6 +99,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format {async = true } end, bufopts)
 end
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local dartBinary
 if vim.fn.has('win32') == 1 then
@@ -70,6 +110,7 @@ end
 
 require('lspconfig').dartls.setup {
   on_attach = on_attach,
+  capabilities = capabilities,
   root_dir = require('lspconfig.util').root_pattern('pubspec.yaml', 'dartdoc_options.yaml'),
   -- https://github.com/dart-lang/sdk/blob/main/pkg/analysis_server/tool/lsp_spec/README.md
   init_options = {
@@ -84,6 +125,35 @@ require('lspconfig').dartls.setup {
     -- Whether to include code snippets (such as class, stful, switch) in code completion. When unspecified, snippets will be included.
     enableSnippets = false,
   },
+}
+
+-- Auto-complete
+local cmp = require 'cmp'
+cmp.setup {
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  })
 }
 
 -- Debug Adapter
