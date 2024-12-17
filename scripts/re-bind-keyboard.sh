058a145270583aee18bc27f2ab18571429446914
Christopher Fujino <christopherfujino@gmail.com>
2024-12-17 09:40:56 -0800

    fiddle with re-bind-keyboard.sh for mac freestyle 2

--- scripts/re-bind-keyboard.sh
+++ scripts/re-bind-keyboard.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+# https://faq.i3wm.org/question/1496/change-caps-lock-to-esc-in-i3.1.html
+setxkbmap -layout us -option caps:escape
+
 if lsusb | grep 'Kinesis Corporation Kinesis Freestyle2 MAC' > /dev/null; then
   # altwin:swap_lalt_lwin Left Alt is swapped with Left Win
   setxkbmap -option altwin:swap_lalt_lwin
-fi
 
-# https://faq.i3wm.org/question/1496/change-caps-lock-to-esc-in-i3.1.html
-setxkbmap -layout us -option caps:escape
+  # https://unix.stackexchange.com/questions/106468/remapping-power-key-to-delete
+  # This must be after any setxkbmap commands(?!)
+  xmodmap -e "keycode 124 = NoSymbol"
+fi
