39b8af502613f666c669facf19b468369dfb1f6d
Christopher Fujino <christopherfujino@gmail.com>
2025-05-23 21:33:22 -0700

    add //scripts/color-test.sh for testing term colors

---
+++ scripts/color-test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+i=0
+
+function color_print() {
+	printf "%2d \e[$1m$2 \e[39mdefault\n" $i
+  i=$(($i + 1))
+}
+
+color_print 30 black
+color_print 31 red
+color_print 32 green
+color_print 33 yellow
+color_print 34 blue
+color_print 35 magenta
+color_print 36 cyan
+color_print 37 'light gray'
+color_print 90 'dark gray'
+color_print 91 'light red'
+color_print 92 'light green'
+color_print 93 'light yellow'
+color_print 94 'light blue'
+color_print 95 'light magenta'
+color_print 96 'light cyan'
+color_print 97 white
