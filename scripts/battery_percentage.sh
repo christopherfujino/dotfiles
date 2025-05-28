32f83a23fe3e8d1e539288fce5ae896a4bb6738d
Christopher Fujino <christopherfujino@gmail.com>
2025-05-27 19:08:42 -0700

    update swayconfig

--- scripts/battery_percentage.sh
+++ scripts/battery_percentage.sh
@@ -13,3 +13,13 @@ BAT_FORMATTED=$(calc -d "$CURRENT_ADJUSTED / $FULL_ADJUSTED * 100" |\
   xargs printf "%.1f%%")
 
 printf "$(date +'%b %d %H:%M') BAT %s" "$BAT_FORMATTED"
+
+# man 7 swaybar-protocol
+#printf '{"version": 1}
+#[
+#  [
+#    {"full_text": "25%%", "min_width": "100%%", "urgent": false},
+#    {"full_text": "123"}
+#  ]
+#]
+#'
