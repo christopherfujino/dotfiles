c394322c8f5030b3b1e26cc85cd999f778c4dbcb
Christopher Fujino <christopherfujino@gmail.com>
2025-06-21 20:43:47 -0700

    update //scripts/screenshot.rb to support wayland

--- scripts/screenshot.rb
+++ scripts/screenshot.rb
@@ -4,22 +4,34 @@ require 'date'
 
 home = ENV['HOME']
 raise 'You do not have a $HOME env var set' if home.nil?
+
 tmp = "#{home}/tmp"
-unless Dir.exists? tmp
+unless Dir.exist? tmp
   puts "Creating temp dir at #{tmp}..."
   Dir.mkdir tmp
 end
 
-filename = "#{DateTime.now.strftime('%Y-%m-%d-%H:%M')}-screenshot.png"
+filename = "#{DateTime.now.strftime('%Y-%m-%d-%H_%M')}-screenshot.png"
 output = "#{tmp}/#{filename}"
 
-result = Kernel.system(
-  'maim',
-  '--hidecursor',
-  '--select', # interactive selection
-  output,
-)
+wayland_display = ENV['WAYLAND_DISPLAY']
+if wayland_display.nil?
+  result = Kernel.system(
+    'maim',
+    '--hidecursor',
+    '--select', # interactive selection
+    output,
+  )
 
-raise "maim failed!" unless result
+  raise "maim failed!" unless result
+else
+  # default 6
+  compression=6
+  result = Kernel.system(
+    "grim -g \"$(slurp)\" -t png -l #{compression} #{output}",
+  )
+
+  raise "maim failed!" unless result
+end
 
 puts "Created file #{output}"
