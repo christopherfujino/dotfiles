70bcfade45a28ace61eb8e2be942615ecb74a1ec
Christopher Fujino <christopherfujino@gmail.com>
2025-06-21 20:38:59 -0700

    Port Neovim config to go (#6)
    * wip
    
    * from init.lua, call client from PATH
    
    * clean up
    
    * update go-client to head
    
    * wip
    
    * figure out mapping to call lua funcs
    
    * move lsp code to go
    
    * re-org
    
    * get plugins ported
    
    * add Makefile

--- init.lua
+++ init.lua
@@ -1,266 +1,69 @@
----- EXPERIMENTAL
+---- Debug Adapter
+---- https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt
+--
+--local dap = require('dap')
+--
+--dap.adapters.dart = {
+--  type = "executable",
+--  command = "dart",
+--  args = {"debug_adapter"}
+--}
+--dap.configurations.dart = {
+--  {
+--    type = "dart",
+--    request = "launch",
+--    name = "Launch Dart Program",
+--    program = "${workspaceFolder}/main.dart",
+--    cwd = "${workspaceFolder}",
+--    --args = {"run", "-d", "web-server"},
+--  }
+--}
+--
+---- See :help dap-widgets
+--local cmd = vim.api.nvim_create_user_command
+--local widgets = require('dap.ui.widgets')
+---- widgets.sidebar could be widgets.centered_float
+--cmd(
+--  'DapShowScopes',
+--  function()
+--    widgets.sidebar(widgets.scopes).open()
+--  end,
+--  {}
+--)
+--cmd(
+--  'DapShowFrames',
+--  function()
+--    widgets.sidebar(widgets.frames).open()
+--  end,
+--  {}
+--)
+--
+---- Syntaxes
+---- autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) | execute 'cd' argv()[0] | endif
+--vim.api.nvim_create_autocmd(
+--  "VimEnter",
+--  {
+--    pattern = "*",
+--    command = "if argc() == 1 && isdirectory(argv()[0]) | execute 'cd' argv()[0] | endif",
+--  }
+--)
 
--- options
-vim.opt.number = true
-vim.opt.tabstop = 2        -- Width of a <tab>
-vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of auto-indent
-vim.opt.expandtab = true   -- Use the appropriate number of spaces to insert a tab
-vim.opt.signcolumn = "yes" -- always draw sign column
-vim.opt.list = true        -- show "-" for trailing whitespace, ">" for tab
-vim.opt.termguicolors = true -- without this, WSL and ssh colors are wack
-vim.opt.ignorecase = true    -- by default, case insensitive search
-vim.opt.smartcase = true     -- if there exist any upper case, search case sensitive
-vim.opt.laststatus = 2     -- always show statusline
-
--- better splits
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-
--- Disable netrw banner
-vim.g.netrw_banner = false
-
----- COMMANDS
-vim.api.nvim_create_user_command(
-  'NVimrc',
-  function(opts)
-    if vim.fn.has("win32") == 1 then
-      vim.cmd("edit ~\\AppData\\Local\\nvim\\init.lua")
+---- https://devforum.roblox.com/t/unnamed-anonymous-functions-give-ambiguous-syntax-errors-if-previous-line-contains-a-function-call/1450059
+f = (function ()
+  if vim.fn.executable('chris-nvim-client') == 1 then
+    local client_job = vim.fn.jobstart(
+      --'/home/chris/git/chris-monorepo/go/nvim-client/run.sh',
+      '/home/chris/git/dotfiles/nvim-client/nvim-client',
+      {rpc = true}
+    )
+    if client_job < 1 then
+      print("Failed to jobstart run.sh")
     else
-      vim.cmd("edit ~/.config/nvim/init.lua")
+      local result = vim.fn.rpcrequest(client_job, 'init', {})
+      print(result)
     end
-  end,
-  {}
-)
-
-vim.api.nvim_create_user_command(
-  'Gl',
-  function(opts)
-    -- note vim buffers don't support terminal escapes, thus no color
-    vim.cmd([[
-      :Git log --format=format:'%h - %cs%d%n          %s - %aN'
-    ]])
-  end,
-  {}
-)
-
-vim.api.nvim_create_user_command(
-  'Gh',
-  function(opts)
-    vim.cmd([[
-      :Git log --graph --all --format=format:'%h - %cs%d%n          %s - %aN'
-    ]])
-  end,
-  {}
-)
-
----- MAPPINGS
-
--- disable middle mouse paste
-vim.keymap.set({"n", "v", "i"}, "<MiddleMouse>", "<Nop>", {noremap=true})
-vim.keymap.set({"n", "v", "i"}, "<2-MiddleMouse>", "<Nop>", {noremap=true})
-vim.keymap.set({"n", "v", "i"}, "<3-MiddleMouse>", "<Nop>", {noremap=true})
-vim.keymap.set({"n", "v", "i"}, "<4-MiddleMouse>", "<Nop>", {noremap=true})
-
-vim.keymap.set("n", "<c-p>", ":Files<cr>", {noremap=true})
-vim.keymap.set("n", "<c-t>", ":tabe<cr>", {noremap=true})
-
----- PLUGINS
-
--- Bootstrap plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup(
-  -- plugins
-  {
-    -- Chris Kempson's Base16 colorschemes, see `colorscheme...`
-    "RRethy/base16-nvim",
-
-    "neovim/nvim-lspconfig",
-    "mfussenegger/nvim-dap",
-    -- Shows git diff in gutter
-    "airblade/vim-gitgutter",
-    -- :Rename, :Move, :Delete
-    "tpope/vim-eunuch",
-    -- cs'"
-    "tpope/vim-surround",
-    -- auto-bracket pairing
-    "windwp/nvim-autopairs",
-
-    -- git
-    'tpope/vim-fugitive',
-    'tpope/vim-rhubarb',
-
-    'junegunn/fzf',
-    'junegunn/fzf.vim',
-
-    'junegunn/goyo.vim',
-  },
-  -- options
-  {}
-)
-
--- This includes newline apparently?!
-vim.g.goyo_width = 81
-
--- For fzf.vim
--- Use rip grep for finding files, respect .gitignore
--- but also search "hidden" files, starting with dot
-vim.fn.setenv("FZF_DEFAULT_COMMAND", "rg --files --hidden")
-
-vim.cmd.colorscheme("base16-eighties")
-
-require("nvim-autopairs").setup {
-  disable_filetype = {},
-}
-
--- lsp
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  -- Will jump to interface files in OCaml
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format {async = true } end, bufopts)
-end
-
-require('lspconfig').dartls.setup {
-  --cmd = { "dart", "language-server", "--protocol=lsp", "--instrumentation-log-file=/usr/local/google/home/fujino/dart-lsp.log" },
-  cmd = {"dart", "language-server", "--protocol=lsp"},
-  on_attach = on_attach,
-  -- https://github.com/dart-lang/sdk/blob/main/pkg/analysis_server/tool/lsp_spec/README.md
-  init_options = {
-    -- When set to true, workspace folders will be ignored and analysis will be performed based on the open files, as if no workspace was open at all. This allows opening large folders without causing them to be completely analyzed. Defaults to false.
-    onlyAnalyzeProjectsWithOpenFiles = true,
-    -- When set to false, completion will not include symbols that are not already imported into the current file. Defaults to true, though the client must additionally support workspace/applyEdit for these completions to be included.
-    suggestFromUnimportedLibraries = true,
-    closingLabels = true,
-    outline = true,
-    flutterOutline = true,
-  },
-  settings = {
-    dart = {
-      showTodos = false,
-      -- Whether to include code snippets (such as class, stful, switch) in code completion. When unspecified, snippets will be included.
-      enableSnippets = false,
-    },
-  },
-}
-
-require('lspconfig').bashls.setup {
-  on_attach = on_attach,
-}
-
--- go install golang.org/x/tools/gopls@latest
-require('lspconfig').gopls.setup {
-  on_attach = on_attach,
-}
-
-require('lspconfig').ts_ls.setup {
-  on_attach = on_attach,
-}
-
--- gem install --user-install ruby-lsp
-require('lspconfig').ruby_lsp.setup {
-  on_attach = on_attach,
-}
-
-require('lspconfig').zls.setup {
-  on_attach = on_attach,
-}
-
-require('lspconfig').ocamllsp.setup {
-  on_attach = on_attach,
-  filetypes = { 'ocaml' },
-  root_dir = require('lspconfig.util').root_pattern('*.opam'),
-}
-
-require('lspconfig').clangd.setup {
-  on_attach = on_attach,
-}
-
-require('lspconfig').cmake.setup {
-  on_attach = on_attach,
-}
-
--- Debug Adapter
--- https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt
-
-local dap = require('dap')
-
-dap.adapters.dart = {
-  type = "executable",
-  command = "dart",
-  args = {"debug_adapter"}
-}
-dap.configurations.dart = {
-  {
-    type = "dart",
-    request = "launch",
-    name = "Launch Dart Program",
-    program = "${workspaceFolder}/main.dart",
-    cwd = "${workspaceFolder}",
-    --args = {"run", "-d", "web-server"},
-  }
-}
-
--- See :help dap-widgets
-local cmd = vim.api.nvim_create_user_command
-local widgets = require('dap.ui.widgets')
--- widgets.sidebar could be widgets.centered_float
-cmd(
-  'DapShowScopes',
-  function()
-    widgets.sidebar(widgets.scopes).open()
-  end,
-  {}
-)
-cmd(
-  'DapShowFrames',
-  function()
-    widgets.sidebar(widgets.frames).open()
-  end,
-  {}
-)
-
--- Syntaxes
--- autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) | execute 'cd' argv()[0] | endif
-vim.api.nvim_create_autocmd(
-  "VimEnter",
-  {
-    pattern = "*",
-    command = "if argc() == 1 && isdirectory(argv()[0]) | execute 'cd' argv()[0] | endif",
-  }
-)
-vim.cmd([[
-autocmd FileType python set shiftwidth=4 tabstop=4
-autocmd FileType cs set shiftwidth=4 tabstop=4
-autocmd FileType go set noexpandtab nosmarttab
-]])
+  else
+    print("[ERROR] You should add `chris-nvim-client` to your path")
+  end
+end)
+f()
---
+++ nvim-client/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/neovim/go-client/nvim"
+)
+
+func setOption(client *nvim.Nvim, name string, value interface{}) {
+	check1(client.SetOptionValue(name, value, map[string]nvim.OptionValueScope{}))
+}
+
+func main() {
+	// TODO make this a stable path
+	// TODO only create this file if in debug mode
+	logFile = check2(os.Create("/home/chris/git/dotfiles/nvim-client/client.log"))
+	defer logFile.Close()
+
+	print("Go entrypoint")
+
+	var reader = os.Stdin
+	var writer = os.Stdout
+	var closer = os.Stdout
+	var client = check2(nvim.New(reader, writer, closer, log.Printf))
+	print("Spawned client")
+	check1(client.RegisterHandler("init", func(c *nvim.Nvim, args []string) (v string, err error) {
+		v = "Success from Go"
+		defer (func() {
+			maybe_err := recover()
+			if maybe_err != nil {
+				print("recovered from panic")
+				err = maybe_err.(error)
+				print("recovered: ")
+				print(err.Error())
+				v = fmt.Sprintf("error from Go: %s", err.Error())
+			}
+		})()
+		print("Handled init RPC call")
+		setOption(c, "number", true)
+		setOption(c, "tabstop", 2)
+		setOption(c, "shiftwidth", 2)
+		setOption(c, "expandtab", true)
+		setOption(c, "signcolumn", "yes")   // Always show sign column, for gitdiff
+		setOption(c, "list", true)          // Show trailing whitespace
+		setOption(c, "termguicolors", true) // Otherwise WSL gets messed up
+		setOption(c, "ignorecase", true)    // case insensitive search...
+		setOption(c, "smartcase", true)     // ...unless you use a capital letter
+		setOption(c, "laststatus", 2)       // Always show status bar
+
+		// Better splits
+		setOption(c, "splitbelow", true)
+		setOption(c, "splitright", true)
+
+		// Disable netrw banner
+		check1(c.SetVar("netrw_banner", false))
+
+		bootstrapPlugins(c)
+
+		check1(c.Command("colorscheme base16-eighties"))
+
+		// FZF
+		var noremap = map[string]bool{"noremap": true}
+		check1(c.SetKeyMap("n", "<c-p>", ":Files<cr>", noremap))
+		check1(c.SetKeyMap("n", "<c-t>", ":tabe<cr>", noremap))
+
+		// Use rip grep for finding files, respect .gitignore
+		// but also search "hidden" files, starting with dot
+		check1(c.Call("setenv", nil, "FZF_DEFAULT_COMMAND", "rg --files --hidden"))
+
+		// LSP Setup
+		lspSetup(c)
+
+		// Disable middle-mouse paste
+		for _, mode := range [3]string{"n", "v", "i"} {
+			check1(c.SetKeyMap(mode, "<MiddleMouse>", "<Nop>", noremap))
+			check1(c.SetKeyMap(mode, "<2-MiddleMouse>", "<Nop>", noremap))
+			check1(c.SetKeyMap(mode, "<3-MiddleMouse>", "<Nop>", noremap))
+			check1(c.SetKeyMap(mode, "<4-MiddleMouse>", "<Nop>", noremap))
+		}
+
+		// Auto-commands
+		check1(c.Command("autocmd FileType python set shiftwidth=4 tabstop=4"))
+		check1(c.Command("autocmd FileType cs set shiftwidth=4 tabstop=4"))
+		check1(c.Command("autocmd FileType go set noexpandtab nosmarttab"))
+		check1(c.Command("autocmd FileType markdown set linebreak"))
+
+		var cmd nvim.UserVimCommand = "edit ~/.config/nvim/init.lua"
+		if runtime.GOOS == "windows" {
+			cmd = "edit ~\\AppData\\Local\\nvim\\init.lua"
+		}
+		c.CreateUserCommand("NVimrc", cmd, map[string]any{})
+
+		// This includes the newline
+		c.SetVar("goyo_width", 81)
+
+		check1(c.ExecLua("require('nvim-autopairs').setup {disable_filetype = {}}", nil, nil))
+
+		return v, err
+	}))
+	check1(client.Serve())
+}
